58db654c3bea9b7ab43945c6ed270930ed7c255c
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-11 13:28:06 +0200

    Ball is bouncing back from the paddles

--- pong.js
+++ pong.js
@@ -23,7 +23,7 @@ Game.prototype.draw = function () {
   this.context.fillRect(this.width/2, 0, 2, this.height);
 
   this.ball.draw(this.context);
-  
+
   this.p1.draw(this.context);
   this.p2.draw(this.context);
 };
@@ -31,6 +31,8 @@ Game.prototype.draw = function () {
 Game.prototype.update = function () {
   if (this.paused)
     return;
+
+  this.ball.update();
 
   if (this.keys.isPressed(83)) {
     this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4);
@@ -44,11 +46,33 @@ Game.prototype.update = function () {
     this.p2.y = Math.max(0, this.p2.y - 4);
   }
 
-  this.ball.update();
-  if (this.ball.x > this.width || this.ball.x + this.ball.width <0) {
-    this.ball.vx = -this.ball.vx;
-  } else if (this.ball.y > this.height || this.ball.y + this.ball.height < 0) {
-    this.ball.vy = -this.ball.vy;
+  if (this.ball.vx > 0) {
+    if (this.p2.x <= this.ball.x + this.ball.width &&
+          this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
+            var collisionDiff = this.ball.x + this.ball.width - this.p2.x;
+            var k = collisionDiff/this.ball.vx;
+            var y = this.ball.vy*k + (this.ball.y - this.ball.vy);
+            if (y >= this.p2.y && y + this.ball.height <= this.p2.y + this.p2.height) {
+              this.ball.x = this.p2.x - this.ball.width;
+              this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy*k);
+              this.ball.vx = -this.ball.vx;
+            }
+          }
+  } else {
+    if (this.p1.x + this.p1.width >= this.ball.x) {
+      var collisionDiff = this.p1.x + this.p1.width - this.ball.x;
+      var k = collisionDiff/-this.ball.vx;
+      var y = this.ball.vy*k + (this.ball.y - this.ball.vy);
+      if (y >= this.p1.y && y + this.ball.height <= this.p1.y + this.p1.height) {
+        this.ball.x = this.p1.x + this.p1.width;
+        this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy*k);
+        this.ball.vx = -this.ball.vx;
+      }
+    }
+  }
+  if ((this.ball.vy < 0 && this.ball.y <0) ||
+          (this.ball.vy > 0 && this.ball.y + this.ball.height > this.height)) {
+      this.ball.vy = -this.ball.vy;
   }
 };
 
